96c8df46081929af3d0660653501c16cbd1a775f
Rüdiger Sonderfeld <ruediger@c-plusplus.net>
2021-09-13 22:11:11 +0100

    Version 1.0-alpha3.

--- qrencode.el
+++ qrencode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Rüdiger Sonderfeld <ruediger@c-plusplus.net>
 ;; Keywords: qrcode comm
-;; Version: 1.0-alpha2
+;; Version: 1.0-alpha3
 ;; Package-Requires: ((emacs "25.1"))
 ;; Package: qrencode
 ;; URL: https://github.com/ruediger/qrencode-el
