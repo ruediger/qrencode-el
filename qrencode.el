11caaf2dc1ac75565b307c6a5ac090dc2ebd51c9
Rüdiger Sonderfeld <ruediger@c-plusplus.net>
2021-09-19 15:41:21 +0100

    Version 1.0 Beta 2.
    A new beta, since 3cb4d12 is a significant change to support larger
    versions.

--- qrencode.el
+++ qrencode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Rüdiger Sonderfeld <ruediger@c-plusplus.net>
 ;; Keywords: qrcode comm
-;; Version: 1.0-beta1
+;; Version: 1.0-beta2
 ;; Package-Requires: ((emacs "25.1"))
 ;; Package: qrencode
 ;; URL: https://github.com/ruediger/qrencode-el
