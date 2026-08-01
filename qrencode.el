677939b0566295e1ff227de115edfcfeceb52bab
Rüdiger Sonderfeld <ruediger@c-plusplus.net>
2026-08-01 11:30:41 +0100

    Version 1.4.
    Changes
    
    * Set line-spacing after enabling major mode by @minad in #24
    * Copyright changed to FSF for elpa inclusion.

--- qrencode.el
+++ qrencode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Rüdiger Sonderfeld <ruediger@c-plusplus.net>
 ;; Keywords: qrcode comm
-;; Version: 1.4-beta1
+;; Version: 1.4
 ;; Package-Requires: ((emacs "25.1"))
 ;; Package: qrencode
 ;; URL: https://github.com/ruediger/qrencode-el
